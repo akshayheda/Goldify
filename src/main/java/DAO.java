863a141776854adca4642cb9916d246fce068fff
Yuval Steinhart <yuval@cs.ucsb.edu>
2019-11-09 20:49:41 -0800

    Added aws SDK

---
+++ src/main/java/DAO.java
@@ -0,0 +1,14 @@
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.regions.Regions;
+public class DAO {
+    public String url;
+
+    public DAO(String url){
+        this.url = url;
+    }
+
+
+
+}
